483aef68f96400b739aa44473b96d2b4f0c89323
tareqhasan382 <tareqhasan382@gmail.com>
2023-12-04 16:00:36 +0600

    test fix issue

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://heliverse-backend-crud.vercel.app",
   }),
   endpoints: () => ({}),
   tagTypes: ["user", "teams"],
